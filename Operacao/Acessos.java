d86f30d59543d498542a728929dfb314b41a2835
Jose Antonio <leao-gomes@hotmail.com>
2022-06-27 21:03:52 -0300

    Iniciando com Scanner

--- Operacao/Acessos.java
+++ Operacao/Acessos.java
@@ -2,6 +2,7 @@ package Operacao;
 
 import Classes.Usuarios;
 import Classes.Contas;
+import java.util.Scanner;
 
 public class Acessos {
     public static void main(String[] args) {
@@ -9,22 +10,29 @@ public class Acessos {
         Usuarios pessoa = new Usuarios();
         Usuarios pessoa1 = new Usuarios();
         Contas contas = new Contas();
+
+        //tentando adicionar scanner para setar o usuario usado
         
+        Scanner entrada = new Scanner(System.in);
 
-        pessoa.setNome("josé ") ;
-        pessoa.setCidade ("Pato Branco ");
-        pessoa.setOcupado (true);
-        pessoa.setResultado(pessoa.isOcupado(pessoa.getOcupado()));
+        System.out.println("Digite o nome do usuario: ");
+        pessoa.setNome(entrada.next());
+        System.out.println(pessoa);
 
-        System.out.println(pessoa.getOcupado() + " " + pessoa.getNome(null) + " " + pessoa.getCidade(null) + contas.getConta1(null) + contas.getSenha1(null) +"\n");
+        // pessoa.setNome("josé ") ;
+        // pessoa.setCidade ("Pato Branco ");
+        // pessoa.setOcupado (true);
+        // pessoa.setResultado(pessoa.isOcupado(pessoa.getOcupado()));
 
-        pessoa1.setNome("Carlos");
-        pessoa1.setCidade("Laranjeiras do sul");
-        pessoa1.setOcupado(false);
-        pessoa1.setResultado(pessoa1.isOcupado(pessoa1.setOcupado(false)));
+        // System.out.println(pessoa.getOcupado() + " " + pessoa.getNome(null) + " " + pessoa.getCidade(null) + contas.getConta1(null) + contas.getSenha1(null) +"\n");
+
+        // pessoa1.setNome("Carlos");
+        // pessoa1.setCidade("Laranjeiras do sul");
+        // pessoa1.setOcupado(false);
+        // pessoa1.setResultado(pessoa1.isOcupado(pessoa1.setOcupado(false)));
 
 
-        System.out.println(pessoa1.getOcupado() + " " + pessoa1.getNome(null) + " " + pessoa1.getCidade(null) + contas.getConta2(null) + contas.getSenha2(null) +"\n");
+        // System.out.println(pessoa1.getOcupado() + " " + pessoa1.getNome(null) + " " + pessoa1.getCidade(null) + contas.getConta2(null) + contas.getSenha2(null) +"\n");
 
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### projeto de estudos java
 
 Este projeto está sendo desenvolvido para consolidar estudos basicos de java. A intenção é futuramente criar interface e um servidor para comunicação entre os clientes.
+
+
+A ser feito:
+
+- [ ] Criar interface para o usuário
+- [ ] Fazer identificador do acesso conforme o nome a pessoa
